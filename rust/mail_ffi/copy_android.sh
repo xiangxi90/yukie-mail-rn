36a4256b03ac8852af27ade17f143001082bfa1f
xiang_xi90 <2651171386@qq.com>
2023-03-27 00:59:48 +0800

    feat: setup project

---
+++ rust/mail_ffi/copy_android.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+mkdir -p ../../android/app/src/main/jniLibs
+mkdir -p ../../android/app/src/main/jniLibs/x86
+mkdir -p ../../android/app/src/main/jniLibs/arm64-v8a
+mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7a
+cp ./target/i686-linux-android/release/libmail_ffi.so ../../android/app/src/main/jniLibs/x86/libmail_ffi.so
+cp ./target/aarch64-linux-android/release/libmail_ffi.so ../../android/app/src/main/jniLibs/arm64-v8a/libmail_ffi.so
+cp ./target/armv7-linux-androideabi/release/libmail_ffi.so ../../android/app/src/main/jniLibs/armeabi-v7a/libmail_ffi.so
